b96c0bf26d61638023c99728453c745857ec01b5
M.S.T.O.P <mstop4@gmail.com>
2017-09-17 11:47:22 -0400

    hello world

--- fmod-studio-sandbox/main.cpp
+++ fmod-studio-sandbox/main.cpp
@@ -1,7 +1,12 @@
+#include <iostream>
 #include "./fmod_studio_api/fmod.hpp"
 #include "./fmod_studio_api/fmod_errors.h"
 
 int main()
 {
+	int foo;
+
+	std::cout << "Hello World!" << std::endl;
+	std::cin >> foo;
 	return 0;
 }
